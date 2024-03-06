76e5894ed471aaf99d3c796ef9077b3d8af6cb8b
minami20230128 <south614fruit@yahoo.co.jp>
2024-03-06 21:46:19 +0900

    ループ使わない方法

--- PowerOfTwo.cpp
+++ PowerOfTwo.cpp
@@ -1,37 +1,15 @@
-#include <iostream>
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        bitset<60> b(n);
 
-class Solution
-{
-    public:
-    bool isPowerOfTwo(int n)
-    {
-        n = n < 0 ? 1 / n : n;
-        bool isPower = true;
-        while(isPower)
-        {
-            //std::cout << n << std::endl;
-            isPower = (this->isMultipleOfTwo(n) || this->isOne(n)) && !this->isZero(n);
-            //std::cout << isPower << std::endl;
-            if(n == 1 || n == 0) break;
-            n = n / 2;
-        }
-
-        return isPower;
-    }
-
-    private: 
-    bool isMultipleOfTwo(int i)
-    {
-        return i % 2 == 0;
-    }
-
-    bool isOne(int i)
-    {
-        return i == 1;
-    }
-
-    bool isZero(int i)
-    {
-        return i == 0;
+        return b.count() == 1;
     }
 };
+
+//解説
+//nを2進数に変換
+//2の乗数であれば、1になっているのは一桁だけのはず
+//1->00000001
+//2->00000010
+//4->00000100
