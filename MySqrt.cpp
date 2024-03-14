7de67ea6e10dfbc0b9870b6c66af938b067906ca
minami20230128 <south614fruit@yahoo.co.jp>
2024-03-14 20:49:45 +0900

    途中

--- MySqrt.cpp
+++ MySqrt.cpp
@@ -1,10 +1,32 @@
 class Solution {
+private:
+    int get_digit(int num)
+    {
+        int digit = 0;
+        while(num >= 10)
+        {
+            num /= 10;
+            digit++;
+        }
+
+        return digit;
+    }
+
+    int get_start_num(int num)
+    {
+        int digit = this->get_digit(num);
+        int power = digit / 2;
+        return std::pow(10, power);
+    }
+
 public:
     int mySqrt(int x) {
-        for(int i = 0; i <= x; i++)
+        int start = get_start_num(x);
+        std::cout << start;
+        for(int i = start; i <= x; i++)
         {
-            long sqrt = i * i;
-            std::cout << sqrt;
+            long sqrt = (long) i * i;
+            //std::cout << sqrt;
             if(sqrt == x) return i;
             if(sqrt > x) return i - 1;
         }
