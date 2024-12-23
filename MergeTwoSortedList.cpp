336d36d42e8f643eba354e101b50dd653877f34c
minami20230128 <south614fruit@yahoo.co.jp>
2024-12-23 23:51:12 +0900

    再帰を使うよう修正

--- MergeTwoSortedList.cpp
+++ MergeTwoSortedList.cpp
@@ -8,62 +8,43 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+					// 😉😉😉😉Please upvote if it 
+helps 😉😉😉😉
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* merged;
-        ListNode* merged_start = merged;
-
-        if(list1 == nullptr) return list2;
-        if(list2 == nullptr) return list1;
-
-        if(list1 < list2)
+	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
+  {
+		// if list1 happen to be NULL
+		// we will simply return list2.
+		if(l1 == NULL)
         {
-            merged = new ListNode(list1->val);
-            list1 = list1->next;
-        }
-        else
+            std::cout << "list1 is null" << std::endl;
+			return l2;
+		}
+		
+		// if list2 happen to be NULL
+		// we will simply return list1.
+		if(l2 == NULL)
         {
-            merged = new ListNode(list2->val);
-            list2 = list2->next;
-        }
-
-        while(1)
+            std::cout << "list2 is null" << std::endl;
+			return l1;
+		} 
+		
+		// if value pointend by l1 pointer is less than equal to 
+value pointed by l2 pointer
+		// we wall call recursively l1 -> next and whole l2 list.
+		if(l1 -> val <= l2 -> val)
         {
-            if(list1 == nullptr)
-            {
-                if(list2 != nullptr)
-                {
-                    merged->next = list2;
-                }
-                return merged_start;
-            }
-
-            if(list2 == nullptr)
-            {
-                if(list1 != nullptr)
-                {
-                    std::cout << list1->val << std::endl;
-                    merged->next = list1;
-                }
-                
-                return merged_start;
-            }
-
-            if(list1->val < list2->val)
-            {
-                merged->next = new ListNode(list1->val);
-                list1 = list1->next;
-                merged = merged->next;
-            }
-            else
-            {
-                merged->next = new ListNode(list2->val);
-                list2 = list2->next;
-                merged = merged->next;
-            }
-        }
-
-        return merged_start;
-    }
-};
+            std::cout << "list1 value" << std::endl;
+			l1 -> next = mergeTwoLists(l1 -> next, l2);
+			return l1;
+		}
+		// we will call recursive l1 whole list and l2 -> next
+		else
+        {
+            std::cout << "list2 value" << std::endl;
+			l2 -> next = mergeTwoLists(l1, l2 -> next);
+			return l2;            
+		}
+	}
+};	
